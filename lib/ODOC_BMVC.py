47d4c1e8774f86127c542da48c27e831ae9565c5
smallmax00 <42297712+smallmax00@users.noreply.github.com>
2021-06-23 15:49:42 +0100

    Add files via upload

---
+++ lib/ODOC_BMVC.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .Res2Net_v1b import res2net50_v1b_26w_4s
+from .edge_aware_gcn import GRU_EAGCN
+
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return x
+
+
+class EDGModule(nn.Module):
+    def __init__(self, channel):
+        super(EDGModule, self).__init__()
+        self.relu = nn.ReLU(True)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_upsample1 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample2 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample3 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+
+        self.conv_1 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_2 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_3 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+
+        self.conv_concat2 = BasicConv2d(2*channel, 2 * channel, 3, padding=1)
+        self.conv_concat3 = BasicConv2d(2*channel, 2 * channel, 3, padding=1)
+        self.conv_concat4 = BasicConv2d(4 * channel, 4 * channel, 3, padding=1)
+
+        self.conv5 = nn.Conv2d(4*channel, 1, 1)
+
+    def forward(self, x1, x2, x3):  # 16x16, 32x32, 64x64
+        up_x1 = self.conv_upsample1(self.upsample(x1))
+        conv_x2 = self.conv_1(x2)
+        cat_x2 = self.conv_concat2(torch.cat((up_x1, conv_x2), 1))
+
+        up_x2 = self.conv_upsample2(self.upsample(x2))
+        conv_x3 = self.conv_2(x3)
+        cat_x3 = self.conv_concat3(torch.cat((up_x2, conv_x3), 1))
+
+        up_cat_x2 = self.conv_upsample3(self.upsample(cat_x2))
+        conv_cat_x3 = self.conv_3(cat_x3)
+        cat_x4 = self.conv_concat4(torch.cat((up_cat_x2, conv_cat_x3), 1))
+        x = self.conv5(cat_x4)
+        return x
+
+
+class SEG_Module(nn.Module):
+
+    def __init__(self, channel):
+        super(SEG_Module, self).__init__()
+        self.relu = nn.ReLU(True)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_upsample1 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample2 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample3 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+
+        self.conv_1 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_2 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_3 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+
+        self.conv_concat2 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+        self.conv_concat3 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+        self.conv_concat4 = BasicConv2d(4 * channel, 4 * channel, 3, padding=1)
+
+
+
+        self.conv5 = nn.Conv2d(4 * channel, 2, 1)
+        self.GCN = GRU_EAGCN(num_in=2, plane_mid=1, mids=32)
+
+    def forward(self, x1, x2, x3, edge):  
+        up_x1 = self.conv_upsample1(self.upsample(x1))
+        conv_x2 = self.conv_1(x2)
+        cat_x2 = self.conv_concat2(torch.cat((up_x1, conv_x2), 1))
+
+        up_x2 = self.conv_upsample2(self.upsample(x2))
+        conv_x3 = self.conv_2(x3)
+        cat_x3 = self.conv_concat3(torch.cat((up_x2, conv_x3), 1))
+
+        up_cat_x2 = self.conv_upsample3(self.upsample(cat_x2))
+        conv_cat_x3 = self.conv_3(cat_x3)
+        cat_x4 = self.conv_concat4(torch.cat((up_cat_x2, conv_cat_x3), 1))
+        seg = self.conv5(cat_x4)
+
+        seg_gcn = self.GCN(seg, edge) 
+        seg_gcn = seg_gcn + seg
+
+        seg = self.upsample(seg_gcn)
+
+
+        return seg
+
+
+class ODOC_seg_edge(nn.Module):
+    
+    def __init__(self, channel=64):
+        super(ODOC_seg_edge, self).__init__()
+       
+        self.resnet = res2net50_v1b_26w_4s(pretrained=False)
+
+        self.rfb2_1 = BasicConv2d(256, channel, 1)
+        self.rfb3_1 = BasicConv2d(512, channel, 1)
+        self.rfb4_1 = BasicConv2d(1024, channel, 1)
+        self.rfb5_1 = BasicConv2d(2048, channel, 1)
+
+        
+        self.edge = EDGModule(channel)
+        
+        self.seg_layer = SEG_Module(channel)
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)      
+
+        
+        x1 = self.resnet.layer1(x)
+        x2 = self.resnet.layer2(x1)     
+        x3 = self.resnet.layer3(x2)     
+        x4 = self.resnet.layer4(x3)     
+
+
+        x1_rfb = self.rfb2_1(x1)
+        x2_rfb = self.rfb3_1(x2)        
+        x3_rfb = self.rfb4_1(x3)        
+        x4_rfb = self.rfb5_1(x4)        
+
+        edge_feat = self.edge(x3_rfb, x2_rfb, x1_rfb)  
+
+        edge = F.interpolate(edge_feat, size=(32, 32), mode='bilinear', align_corners=True)
+
+        seg = self.seg_layer(x4_rfb, x3_rfb, x2_rfb, edge)  
+
+        seg_output = torch.sigmoid(seg)
+
+
+        return seg_output, torch.sigmoid(edge_feat)
+
+
---
+++ lib/edge_aware_gcn.py
@@ -0,0 +1,122 @@
+import torch.nn as nn
+import torch
+
+
+class GCN(nn.Module):
+    def __init__(self, num_state, num_node):
+        super(GCN, self).__init__()
+        self.num_state = num_state
+        self.num_node = num_node
+        self.conv1 = nn.Conv1d(num_node, num_node, kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv1d(num_state, num_state, kernel_size=1)
+
+    def forward(self, seg, aj):
+        n, c, h, w = seg.size()
+        seg = seg.view(n, self.num_state, -1).contiguous()
+        seg_similar = torch.bmm(seg, aj)
+        out = self.relu(self.conv2(seg_similar))
+        output = out + seg
+       
+        return output
+
+
+class EAGCN(nn.Module):
+    def __init__(self, num_in, plane_mid, mids, normalize=False):  
+        super(EAGCN, self).__init__()
+        self.num_in = num_in
+        self.mids = mids
+        self.normalize = normalize
+        self.num_s = int(plane_mid)
+        self.num_n = (mids) * (mids)
+        self.maxpool_c = nn.AdaptiveAvgPool2d(output_size=(1))
+        self.conv_s1 = nn.Conv2d(num_in, self.num_s, kernel_size=1)
+        self.conv_s11 = nn.Conv2d(num_in, self.num_s, kernel_size=1)
+        self.conv_s2 = nn.Conv2d(1, 1, kernel_size=1)
+        self.conv_s3 = nn.Conv2d(1, 1, kernel_size=1)
+        self.mlp = nn.Linear(num_in, self.num_s)
+        self.fc = nn.Conv2d(num_in, self.num_s, 1, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.downsample = nn.AdaptiveAvgPool2d(output_size=(mids, mids))
+
+
+        self.gcn = GCN(num_state=num_in, num_node=self.num_n)
+        self.conv_extend = nn.Conv2d(self.num_s, num_in, kernel_size=1)
+        self.blocker = nn.BatchNorm2d(num_in)
+
+    def forward(self, seg_ori, edge_ori): 
+
+
+        seg = seg_ori 
+        edge = edge_ori 
+        n, c, h, w = seg.size()
+
+        
+        seg_s = self.conv_s1(seg)
+        theta_T = seg_s.view(n, self.num_s, -1).contiguous() 
+        theta = seg_s.view(n, -1, self.num_s).contiguous() 
+        channel_att = torch.relu(self.mlp(self.maxpool_c(seg).squeeze(3).squeeze(2))).view(n, self.num_s, -1)  
+        diag_channel_att = torch.bmm(channel_att, channel_att.view(n, -1, self.num_s)) 
+
+        similarity_c = torch.bmm(theta, diag_channel_att) 
+        similarity_c = self.softmax(torch.bmm(similarity_c, theta_T))  
+        
+
+        seg_c = self.conv_s11(seg)
+        sigma = seg_c.view(n, self.num_s, -1).contiguous()
+        sigma_T = seg_c.view(n, -1, self.num_s).contiguous()
+        sigma_out = torch.bmm(sigma_T, sigma)
+
+        edge_m = seg * edge
+
+        maxpool_s, _ = torch.max(seg, dim=1)
+        edge_m_pool, _ = torch.max(edge_m, dim=1)
+
+        seg_ss = self.conv_s2(maxpool_s.unsqueeze(1)).view(n, 1, -1)  
+        edge_mm = self.conv_s3(edge_m_pool.unsqueeze(1)).view(n, -1, 1)
+
+        diag_spatial_att = torch.bmm(torch.bmm(edge_mm, seg_ss), sigma_out)
+        similarity_s = self.softmax(diag_spatial_att)
+        similarity = similarity_c + similarity_s
+
+
+        seg_gcn = self.gcn(seg, similarity).view(n, self.num_in, self.mids, self.mids)
+
+        ext_up_seg_gcn = seg_gcn + seg_ori
+        return ext_up_seg_gcn
+
+
+class GRU_EAGCN(nn.Module):
+    def __init__(self, num_in, plane_mid, mids):
+        super(GRU_EAGCN, self).__init__()
+
+        self.eagcn = EAGCN(num_in, plane_mid, mids)
+        self.rnn = torch.nn.GRU(input_size=1024, hidden_size=1024, num_layers=1)
+    def forward(self, seg, edge):
+        _, c, h, w = seg.size()
+        #------------t0-------#
+        updated_seg = self.eagcn(seg, edge)
+        updated_seg = updated_seg.view(c, -1, h*w)
+        output_0, h_0 = self.rnn(updated_seg)
+        # ------------t1-------#
+        output = output_0.view(-1, c, h, w)
+        updated_seg = self.eagcn(output, edge)
+        updated_seg = updated_seg.view(c, -1, h*w)
+        output_1, h_1 = self.rnn(updated_seg, h_0)
+        #-------------t2----------#
+        output = output_1.view(-1, c, h, w)
+        updated_seg = self.eagcn(output, edge)
+        updated_seg = updated_seg.view(c, -1, h*w)
+        output_2, h_2 = self.rnn(updated_seg, h_1)
+
+        # reshape back
+        output_2 = output_2.view(-1, c, h, w)
+
+        return output_2
+
+
+
+
+
+
+

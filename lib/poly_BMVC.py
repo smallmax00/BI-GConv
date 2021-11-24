a9d0bfb3ce8cebdf683afbe29d89b6dcad60319b
Yanda Meng <42297712+smallmax00@users.noreply.github.com>
2021-11-24 11:02:25 +0000

    Update poly_BMVC.py

--- lib/poly_BMVC.py
+++ lib/poly_BMVC.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .Res2Net_v1b import res2net50_v1b_26w_4s
+from .Res2Net_v1b import res2net50_v1b_26w_4s 
 from .edge_aware_gcn import GRU_EAGCN
 
 
